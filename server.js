b4cefbf4f2ca4140354b0ff9df8d4c879fb28f34
Nick El Greco Bimpasis <nikolaosb@gmail.com>
2013-09-18 11:29:49 -0500

    step 4 - socket.io

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
 function server(req, res) {
-    if (req.url == "/") {
-        ASQ()
-            .then(function (done) {
-                setTimeout(function () {
-                    done(Math.random(10));
-                }, 1000);
-            })
-            .then(function (_, msg) {
-                res.writeHead(200, { "Content-type":"text/plain" });
-                res.end("Hello there " + msg);
-            })
-    }
-    else if ((/^\/\d+(?=$|[\/?#])/).test(req.url)) {
+    if ((/^\/\d+(?=$|[\/?#])/).test(req.url)) {
         req.addListener("end", function(){
             req.url = req.url.replace(/^\/(\d+).*$/, "$1.html");
             static_files.serve(req, res);
@@ -22,9 +10,18 @@ function server(req, res) {
 
 var http = require("http"),
     httpServer = http.createServer(server),
-    port = 8005
-var ASQ = require("asynquence"); // installed with "npm install asynquence" in bash
-var node_static = require("node-static"); // installed with "npm install asynquence" in bash
-var static_files = new node_static.Server(__dirname);
+    port = 8005,
+    ASQ = require("asynquence"), // installed with "npm install asynquence" in bash
+    node_static = require("node-static"), // installed with "npm install asynquence" in bash
+    static_files = new node_static.Server(__dirname),
+    io = require("socket.io").listen(httpServer)
+    ;
+
+io.configure(function(){
+    io.enable("browser client minification");
+    io.enable("browser client etag");
+    io.set("log level", 1);
+    io.set("transports", ["websocket", "xhr-polling", "jsonp-polling"]);
+});
 
 httpServer.listen(port);
