c3db97550463212bf1cfff7885ef131112afa184
Nick El Greco Bimpasis <nikolaosb@gmail.com>
2013-09-18 11:13:52 -0500

    step 3

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 function server(req, res) {
-  if (req.url == "/") {
-    ASQ()
-        .then(function(done){
-            setTimeout(function(){
-                done(Math.random(10));
-            }, 1000);
-        })
-        .then(function(_, msg){
-            res.writeHead(200, { "Content-type": "text/plain" })
-            res.end("Hello there " + msg);
-        })
+    if (req.url == "/") {
+        ASQ()
+            .then(function (done) {
+                setTimeout(function () {
+                    done(Math.random(10));
+                }, 1000);
+            })
+            .then(function (_, msg) {
+                res.writeHead(200, { "Content-type":"text/plain" });
+                res.end("Hello there " + msg);
+            })
+    }
+    else if ((/^\/\d+(?=$|[\/?#])/).test(req.url)) {
+        req.addListener("end", function(){
+            req.url = req.url.replace(/^\/(\d+).*$/, "$1.html");
+            static_files.serve(req, res);
+        });
+        req.resume();
     }
 }
 
 var http = require("http"),
     httpServer = http.createServer(server),
     port = 8005
-    ASQ = require("asynquence"); // installed with "npm install asynquence" in bash
+var ASQ = require("asynquence"); // installed with "npm install asynquence" in bash
+var node_static = require("node-static"); // installed with "npm install asynquence" in bash
+var static_files = new node_static.Server(__dirname);
 
 httpServer.listen(port);
